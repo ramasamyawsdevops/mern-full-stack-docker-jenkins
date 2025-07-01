9231f0d87f603ee2e446bc34309cac748889eb93
ramasamyawsdevops <ramasamyawsdevops@gmail.com>
2025-07-01 22:18:14 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectToMongoDB = () => {
     try {
-        mongoose.connect(process.env.MONGO_DB_URL);
+        mongoose.connect(process.env.MONGO_URL);
         console.log("Connected to MongoDB Successfully!!");
     } catch (e) {
         console.log(e.message);
